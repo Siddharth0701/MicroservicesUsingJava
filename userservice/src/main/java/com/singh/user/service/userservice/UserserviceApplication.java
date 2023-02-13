4d5207d92f523c801cf1f30c497f9dc9060d8df5
Siddharth0701 <siddharth.singh0701@gmail.com>
2023-02-13 11:02:34 +0530

    implemented user service on eureka server using netflix server

--- userservice/src/main/java/com/singh/user/service/userservice/UserserviceApplication.java
+++ userservice/src/main/java/com/singh/user/service/userservice/UserserviceApplication.java
@@ -2,13 +2,15 @@ package com.singh.user.service.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserserviceApplication.class, args);
-		System.out.println("........................Project started...............");
+		//System.out.println("........................Project started...............");
 	}
 
 }
