ffd04f2a266c51ff91da62a123ad9d5b8490ce37
Siddharth0701 <siddharth.singh0701@gmail.com>
2023-02-14 18:16:05 +0530

    implemented Config Client

--- RatingService/src/main/java/com/redlabel/rating/RatingServiceApplication.java
+++ RatingService/src/main/java/com/redlabel/rating/RatingServiceApplication.java
@@ -3,8 +3,6 @@ package com.redlabel.rating;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,6 +10,7 @@ public class RatingServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RatingServiceApplication.class, args);
+		
 
 	}
 
