d6969183af98f386f64860da950ad1f6eb16b1ef
Siddharth0701 <siddharth.singh0701@gmail.com>
2023-02-15 20:30:17 +0530

    implemented OKTA in ApiGateway

---
+++ ApiGateway/src/main/java/com/redlabel/getwway/models/AuthResponse.java
@@ -0,0 +1,26 @@
+package com.redlabel.getwway.models;
+
+import java.util.Collection;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class AuthResponse {
+    private String userId;
+    private String accessToken;
+    private String refreshToken;
+    private long expireAt;
+    private Collection<String> authorities;
+
+}
