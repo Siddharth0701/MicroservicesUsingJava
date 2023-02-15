4d4707f17392e5f296d8397f06853dba6417cfb7
Siddharth0701 <siddharth.singh0701@gmail.com>
2023-02-15 14:44:13 +0530

    implement retrying of request from one service to another service with help of @Retry

--- UserServices/src/main/java/com/redlabel/user/service/controller/UserController.java
+++ UserServices/src/main/java/com/redlabel/user/service/controller/UserController.java
@@ -18,6 +18,7 @@ import com.redlabel.user.service.entities.User;
 import com.redlabel.user.service.services.impl.UserServiceImpl;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 @RequestMapping("api/users")
@@ -40,22 +41,29 @@ public class UserController {
         return ResponseEntity.ok(allUser);
 
     }
+    int retryCount=1;
 
     @GetMapping("{userId}")
-    @CircuitBreaker(name = "ratingHoterBreaker", fallbackMethod = "ratingHotelFallback")
+    // @CircuitBreaker(name = "ratingHoterBreaker", fallbackMethod =
+    // "ratingHotelFallback")
+    @Retry(name = "ratingHotelService", fallbackMethod = "ratingHotelFallback")
     // getSingleUser
     public ResponseEntity<User> getSingleUser(@PathVariable String userId) {
         logger.info("Get Single User Handler: UserController");
+        logger.info("Retry count:{}", retryCount);
+        retryCount++;
         User user = userserviceImpl.getUser(userId);
         return ResponseEntity.ok(user);
 
     }
-    //created fall back method for circuit braker
-    public ResponseEntity<User> ratingHotelFallback( String userId, Exception ex ){
-        logger.info("Fallback is executed because service is down : ", ex.getMessage());
-        User user = User.builder().email("dummy@gmail.com").name("Dummy").about("This user is created dummy because some service is down").userId("141234").build();
-        return new ResponseEntity<>(user,HttpStatus.OK);
 
+    // created fall back method for circuit braker
+   
+    public ResponseEntity<User> ratingHotelFallback(String userId, Exception ex) {
+        //logger.info("Fallback is executed because service is down : ", ex.getMessage());
+        User user = User.builder().email("dummy@gmail.com").name("Dummy")
+                .about("This user is created dummy because some service is down").userId("141234").build();
+        return new ResponseEntity<>(user, HttpStatus.OK);
 
     }
 
