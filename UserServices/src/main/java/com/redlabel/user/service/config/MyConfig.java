eed71a7b9eb1ec5ac4808ea5c25c6c83839f2e56
Siddharth0701 <siddharth.singh0701@gmail.com>
2023-02-17 13:17:35 +0530

    Implemented FeignClientIntercepter

--- UserServices/src/main/java/com/redlabel/user/service/config/MyConfig.java
+++ UserServices/src/main/java/com/redlabel/user/service/config/MyConfig.java
@@ -4,7 +4,16 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.ser.std.StdKeySerializers.Default;
 
 @Configuration
 
@@ -13,5 +22,19 @@ public class MyConfig {
     @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    // declaare bean of OAuth2AuthorizedClientmanger
+    @Bean
+    public OAuth2AuthorizedClientManager manager(
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository auth2AuthorizedClientRepository) {
+        OAuth2AuthorizedClientProvider provider = OAuth2AuthorizedClientProviderBuilder.builder().clientCredentials()
+                .build();
+        DefaultOAuth2AuthorizedClientManager defaultOAuth2AuthorizedClientManager = new DefaultOAuth2AuthorizedClientManager(
+                clientRegistrationRepository, auth2AuthorizedClientRepository);
+        defaultOAuth2AuthorizedClientManager.setAuthorizedClientProvider(provider);
+        return defaultOAuth2AuthorizedClientManager;
+
     }
 }
