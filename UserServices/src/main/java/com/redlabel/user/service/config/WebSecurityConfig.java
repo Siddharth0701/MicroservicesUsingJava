a52261eaa2606be2f0e47a7080b913afb919a5a0
Siddharth0701 <siddharth.singh0701@gmail.com>
2023-02-20 20:41:02 +0530

    Project completed

--- UserServices/src/main/java/com/redlabel/user/service/config/WebSecurityConfig.java
+++ UserServices/src/main/java/com/redlabel/user/service/config/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package com.redlabel.user.service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,12 +14,14 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
+
         security.authorizeHttpRequests()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .oauth2ResourceServer()
                 .jwt();
+
         return security.build();
 
     }
